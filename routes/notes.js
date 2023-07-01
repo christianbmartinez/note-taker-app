afd699a979e879b5930a83d22da64b337a85ce83
Christian Martinez <christian@mixdlabs.com>
2023-07-01 12:55:20 -0600

    Updated notations, we can successfully save a note to the database and assign a unique id to the newNote object using uuid.

--- routes/notes.js
+++ routes/notes.js
@@ -1,18 +1,33 @@
+// Packages needed for our api
 const notes = require('express').Router()
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils')
-//const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 
-// GET Route for retrieving all the notes
+// GET API Route for retrieving all the notes
 notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
-// POST Route for creating a new note
+// POST API Route for creating a new note
 notes.post('/', (req, res) => {
-  console.log(req.body)
+  if (req.body) {
+    const { title, text } = req.body
+
+    const newNote = {
+      title,
+      text,
+      // uuid generates a unique id for each note
+      id: uuidv4(),
+    }
+
+    readAndAppend(newNote, './db/db.json')
+    res.json({ success: true, text: 'Successfully added new note' })
+  } else {
+    res.json({ success: false, text: 'Failed to add new note' })
+  }
 })
 
-// DELETE Route to delete a note
+// DELETE API Route to delete a note
 notes.delete('/', (req, res) => {
   console.log(req.body)
 })
